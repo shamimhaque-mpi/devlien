4ecc85efbf5032a4ba5ebbf49a33da7f9cc840d5
Shamim Haque <shamim.haque.dev@gmail.com>
2025-07-13 09:46:23 +0600

    Update Nuxt Api

--- libraries/nuxt/api/[...slug].js
+++ libraries/nuxt/api/[...slug].js
@@ -1,12 +1,5 @@
 import run from "deepline/run";
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
-
-
-export default async function handler(req, res) {
-  res.status(200).json((await (new run(req)).getResponse()));
-}
+export default defineEventHandler(async (event) => {
+  return (new run(event.node.req)).getResponse();
+})
