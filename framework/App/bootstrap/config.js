6dd3c0b5f84096ce8b31e6615197aefb59ce1234
Shamim Haque <shamim.haque.dev@gmail.com>
2025-07-25 01:34:58 +0600

    bootstrap have been cleaned

--- framework/App/bootstrap/config.js
+++ framework/App/bootstrap/config.js
@@ -1,10 +1,1 @@
-import System from "devlien/system";
-
-const app = (await import('file:///Users/shamimhaque/Desktop/Nuxt/my-app/server/config/app.js')).default;
-const database = (await import('file:///Users/shamimhaque/Desktop/Nuxt/my-app/server/config/database.js')).default;
-
-
-export const configs = {
-  app : app,
-  database : database,
-}
+export const configs = {}
